a3b21dd5f5038aa0e42fb34ce39fe72d6033f8db
HP <dhanukapps@gmail.com>
2020-12-24 21:28:45 +0530

    updated

--- src/main/java/com/httpserver/httpserver.java
+++ src/main/java/com/httpserver/httpserver.java
@@ -3,6 +3,6 @@ package com.httpserver;
 public class httpserver {
 
     public static void main(String args[]){
-            System.out.println("Server is starting");
+            System.out.println("Server is starting......");
     }
 }
